b0038fab3cde98dbdd0ccb1c00665d14831b67ee
pon4ikisdonut <pontyarliliya@gmail.com>
2026-01-17 02:00:43 +0200

    Disabled debug mode

--- src/kernel/epstein.c
+++ src/kernel/epstein.c
@@ -1,4 +1,4 @@
 #include "epstein.h"
 
-int epstein = 1; // Default to 0, meaning debug features are disabled
+int epstein = 0; // Default to 0, meaning debug features are disabled
 char epstein_files[] = ""; // Default message
